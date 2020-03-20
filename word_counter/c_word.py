7cd8df00b7efe72e52ca7559b4e645ac3dd9c4c9
lanodiware <40364398+lanodiware@users.noreply.github.com>
2020-03-20 17:46:34 +0100

    word_count

---
+++ word_counter/c_word.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+def strip_symbol(parola):
+    symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '_', '+', '=', '{', '}', ']', '[', '|',
+               '\\', '`', ',', '.', '/', '?', ';', ':', '\'', '"', '<', '>']
+    parola = parola.strip()
+    for x in symbols:
+        parola = parola.replace(x, '')
+    return parola
+
+def word_counter(bookpath):
+    with open(bookpath, 'r') as f:
+        book = f.read().split(' ')
+        f.close()
+    words = {}
+    for i in book:
+        i = strip_symbol(i)
+        if i != '':
+            if i not in words:
+                words[i] = 1
+            else:
+                words[i] += 1
+
+    sorted_words = {k: v for k, v in sorted(words.items(), key=lambda item: item[1], reverse=True)}
+
+    return sorted_words
+
+
+with open('dizionario_italiano.txt', 'r') as f:
+    dizionario = f.read().split('\n')
+    f.close()
+
+counted = word_counter('nuova_riveduta.txt')
+
+
+count = 0
+for i in counted:
+    if i.lower() in dizionario and len(i) > 3 :
+        print(str(i) + "\t" + str(counted[i]))
+        count += 1
+        if count > 10:
+            break
