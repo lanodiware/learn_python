2f27bb7d7d4d5b83b5f0f54f1fe1b2230e373284
lanodiware <40364398+lanodiware@users.noreply.github.com>
2020-07-06 06:50:55 +0200

    Update main.py

--- matrix_test/main.py
+++ matrix_test/main.py
@@ -1,29 +1,50 @@
 # coding : utf-8
 
-from time import sleep 
-from random import randint 
+from random import randint
+from time import sleep
+import os
 
-matrix = [[[],[],[]],[[],[],[]],[[],[],[]]]
-numbers = [1,2,3,4,5,6,7,8,9]
+def check_mat(m):
+    counter = {
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0,
+        "7": 0,
+        "8": 0,
+        "9": 0
+    }
+    counters = []
+    for rows in m:
+        for i in range(1, 10):
+            if i in rows:
+            	c = counter
+                for x in rows:
+                    if x == i:
+                        c[str(i)] += 1
 
-def gen_sudoku(mat):
-	# cicla quadrati della matrice 
-	for squares in mat : 
-		alrdch = []
-		# cicla righe dei quadrati 
-		for row in squares :
-			# riempi le righe con 3 numeri random 
-			i = 0
-			while i < 3 :			
-				rnd = randint(1,9)
-				if rnd not in row and rnd not in alrdch : 
-					row.append(rnd)
-					alrdch.append(rnd)
-					i += 1
-				else:
-					continue
-	return(mat)
-
-print(gen_sudoku(matrix))
+        print("Conteggio riga:", rows)
+        print(c)
+        sleep(1)
 
 
+def pop_matrix():
+    m = []
+    for i in range(0, 9):
+        rows = []
+        for x in range(0, 9):
+            rows.append(randint(1, 9))
+        m.append(rows)
+    return m
+
+def print_mat(m):
+    print("Matrice:")
+    for i in m:
+        print(i)
+
+matrix = pop_matrix()
+print_mat(matrix)
+check_mat(matrix)
+#print_mat(matrix)
