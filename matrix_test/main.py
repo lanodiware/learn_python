90e2dc320a01762f169f015d285ed23a9bda67bf
lanodiware <40364398+lanodiware@users.noreply.github.com>
2020-07-08 10:00:35 +0200

    Update main.py

--- matrix_test/main.py
+++ matrix_test/main.py
@@ -5,6 +5,9 @@ from time import sleep
 import os
 import copy
 
+change = True
+
+
 def check_mat(m):
     counter = {
         "1": 0,
@@ -17,51 +20,83 @@ def check_mat(m):
         "8": 0,
         "9": 0
     }
-    counters_r = []
-    counters_c = []
-    counters_q = []
+    counters = []
+
     for i in range(0, 9):
-        counters_r.append(copy.deepcopy(counter))
-        counters_c.append(copy.deepcopy(counter))
-        counters_q.append(copy.deepcopy(counter))
+        counters.append(copy.deepcopy(counter))
 
     # righe
     for i in range(0, 9):
         # numeri
         for x in m[i]:
-            counters_r[i][str(x)] += 1
-
-    numat = []
-    # flip table
-    for i in range(0, 9):
-        nurow = []
-        for x in range(0, 9):
-            nurow.append(m[x][i])
-        numat.append(nurow)
+            counters[i][str(x)] += 1
 
     # colonne
+    for i in range(1, 10):
+        for rows in m:
+            counters[i-1][str(rows[i-1])] += 1
+
+    # quadrati
+    q1 = 0
+    q2 = 3
+    divider = 0
+    list_q = []
+    quad = []
+    for j in range(0,7,3):
+        for rows in m:
+            for i in range(q1+j, q2+j):
+                quad.append(rows[i])
+            divider += 1
+            if divider % 3 == 0:
+                list_q.append(quad)
+                quad = []
+
+    # count q
     for i in range(0, 9):
         # numeri
-        for x in numat[i]:
-            counters_c[i][str(x)] += 1
+        for x in list_q[i]:
+            counters[i][str(x)] += 1
 
-    # quadrati 
+    change = False
 
-def pop_matrix():
-    m = []
     for i in range(0, 9):
-        rows = []
         for x in range(0, 9):
-            rows.append(randint(1, 9))
-        m.append(rows)
+            if m[i][x] != 0:
+                if counters[i][str(m[i][x])] > 1:
+                    m[i][x] = 0
+                    change = True
+    return m
+
+
+def pop_matrix(m):
+    for i in range(0, 9):
+        for x in range(0, 9):
+            if m[i][x] == 0:
+                m[i][x] = randint(1, 9)
     return m
 
 def print_mat(m):
-    print("Matrice:")
     for i in m:
         print(i)
 
-matrix = pop_matrix()
+
+def init_matrix():
+    matrix = []
+    for i in range(0, 9):
+        rows = []
+        for x in range(0, 9):
+            rows.append(0)
+        matrix.append(rows)
+    return matrix
+
+matrix = init_matrix()
+lav = 0
+while change:
+    matrix = pop_matrix(matrix)
+    #print_mat(matrix)
+    matrix = check_mat(matrix)
+    lav += 1
+    print("Lavoro...", lav)
+
 print_mat(matrix)
-check_mat(matrix)
-#print_mat(matrix)
+
