cc26ee314882323c02eb46e6c3f6f91c783caf98
lanodiware <40364398+lanodiware@users.noreply.github.com>
2020-08-21 17:08:19 +0200

    il computer ora non mette sempre a caso

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -44,9 +44,46 @@ def sceltains():
     return [sceltay, sceltax]
 
 
+def smarty(mat):
+    flipmat = []
+    for i in range(0,3):
+        rows = []
+        for x in range(0,3):
+            rows.append(mat[x][i])
+        flipmat.append(rows)
+
+    for i in range(0,3):
+        if mat[i].count('O') == 2 and '' in mat[i]:
+            return [i, mat[i].index('')]
+
+    for i in range(0, 3):
+        if flipmat[i].count('O') == 2 and '' in flipmat[i]:
+            return [flipmat[i].index(''), i]
+
+    d1 = [mat[0][0], mat[1][1], mat[2][2]]
+    d2 = [mat[2][0], mat[1][1], mat[0][2]]
+
+    if d1[0] == 'O' and d1[1] == 'O' and d1[2] == '' :
+        return [2, 2]
+    if d1[0] == 'O' and d1[1] == '' and d1[2] == 'O' :
+        return [1, 1]
+    if d1[0] == '' and d1[1] == 'O' and d1[2] == 'O':
+        return [0, 0]
+
+    if d2[0] == 'O' and d2[1] == 'O' and d2[2] == '' :
+        return [0, 2]
+    if d2[0] == 'O' and d2[1] == '' and d2[2] == 'O' :
+        return [1, 1]
+    if d2[0] == '' and d2[1] == 'O' and d2[2] == 'O':
+        return [2, 0]
+
+
 def pcchoose(mat):
     while True:
         x, y = randint(0,2), randint(0,2)
+        sm = smarty(mat)
+        if sm is not None:
+            x, y = sm[0], sm[1]
         if matgame[x][y] == '':
             matgame[x][y] = 'O'
             break
@@ -96,6 +133,7 @@ def winner(mat):
         w = 'O'
     return w
 
+
 def aretherespaces(mat):
     space = False
     for row in mat:
@@ -107,15 +145,12 @@ def aretherespaces(mat):
 playing = True
 firsttime = True
 while playing:
-
-
     if firsttime and randint(0,1) :
         print("Comincia il pc...")
         matgame = pcchoose(matgame).copy()
         printmat(matgame)
 
     firsttime = False
-
     sceltaok = True
     while sceltaok:
         p = sceltains()
